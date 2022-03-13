bb79a97f2e7051055bcc08a6bd4fe7b77e9130ea
Blacknotez <94313859+Blacknotez@users.noreply.github.com>
2022-03-13 19:17:46 +0530

    Update insta.js
    insta updated

--- plugins/insta.js
+++ plugins/insta.js
@@ -10,12 +10,6 @@ const ph = "instagram Media Downloads."
 const { errorMessage, infoMessage } = require('../helpers')
 const Language = require('../language');
 const Lang = Language.getString('instagram')
-const s = require('../config');
-const v = s.CHANNEL
-const need = "*_Need instagram link!_*";
-const need_acc = "*_Need an instagram username!_*";
-const fail = "*_Download failed! Check your link and try again_*";
-const need_acc_s = "_Need an instagram username or link!_";
 
 
 Asena.addCommand({ pattern: 'vinsta ?(.*)', fromMe: false, desc: sd }, async (message, match) => {
@@ -130,25 +124,3 @@ Asena.addCommand({ pattern: 'igstalk ?(.*)', fromMe: true, desc: Lang.DESC }, (a
         });
 
 }));
-Asena.addCommand({ pattern: 'ig ?(.*)', fromMe: sourav,dontAddCommandList: true }, (async (msg, query) => {
-    if (query[1] === '') return await msg.client.sendMessage(msg.jid, need_acc, MessageType.text, {quoted: msg.data});
-    var res = await raganork.query.getStalk(query[1])
-    if (res === "false") return await msg.client.sendMessage(msg.jid, "_Username invalid!_", MessageType.text, {quoted: msg.data})
-    var buffer = await raganork.query.skbuffer(res.user.hd_profile_pic_url_info.url)
-    await msg.client.sendMessage(msg.jid, buffer, MessageType.image, { mimetype: Mimetype.jpg, caption: '_*Name:*_ ' + `${res.user.full_name}` + '\n _*Bio:*_ ' + `${res.user.biography}`+ '\n _*Private account:*_ ' + `${res.user.is_private} ` + '\n _*Followers:*_ ' + `${res.user.follower_count}` + '\n _*Following:*_ ' + `${res.user.following_count}` + '\n _*Posts:*_ ' + `${res.user.media_count}` + '\n _*Verified:*_ ' + `${res.user.is_verified} ` + '\n _*IGTV videos:*_ ' + `${res.user.total_igtv_videos}`, quoted: msg.data});
-    }));
-skl.addCommand({ pattern: 'story ?(.*)', fromMe: sourav,dontAddCommandList: true }, (async (msg, query) => {
-if (query[1] === '') return await msg.client.sendMessage(msg.jid, need_acc_s, MessageType.text, {quoted: msg.data});
-var user = query[1];
-var res = await raganork.query.getStory(user,v)
-if (res === "false") return await msg.client.sendMessage(msg.jid, "_Story not found!_", MessageType.text, {quoted: msg.data})
-var url = ''
-res.result.stories.map((result) => {
-url += result.url + ','});
-var que = url !== false ? url.split(',') : [];
-for (var i = 0; i < (que.length < res.result.stories.length ? que.length : res.result.stories.length); i++) {
-var get = got(que[i], {https: {rejectUnauthorized: false}});
-var stream = get.buffer();
-stream.then(async (video) => {
-await msg.client.sendMessage(msg.jid, video, MessageType.video, { mimetype: Mimetype.mp4, caption: '```Story of '+res.result.username + '```', quoted: msg.data});
-})};
