61fcc9846b1b52d46c06146ebdc2d00b58ff4705
Blacknotez <94313859+Blacknotez@users.noreply.github.com>
2022-03-13 20:51:50 +0530

    Update truecaller.js

--- plugins/truecaller.js
+++ plugins/truecaller.js
@@ -3,8 +3,10 @@ const Asena = require('../events');
 const config = require('../config');
 const {MessageType} = require('@adiwajshing/baileys');
 const v = s.CHANNEL
-const sourav = s.WORKTYPE == 'public' ? false : true
-New.addCommand({pattern: 'true ?(.*)', desc: 'Searches for number in truecaller!',fromMe: true}, async (msg, query) => {
+
+
+if (Config.WORKTYPE == 'private') {
+Asena.addCommand({pattern: 'true ?(.*)', desc: 'Searches for number in truecaller!',fromMe: true}, async (msg, query) => {
 	if (!query[1] && !msg.reply_message) return await msg.reply("_Give me any number or reply to any user!_");
 	if (query[1].includes('/')) return await msg.client.sendMessage(msg.jid, 'Wrong format! \n\n .true +91 6282344739', MessageType.text, {quoted: msg.data})
 	var go;
